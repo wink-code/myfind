afb13a2914fbfafdd1927219f9423d24cbd65874
johnwink <johnwink1998@gmail.com>
2026-09-14 16:49:02 +0800

    change the version and change the unique version source

--- src/myfind/_meta.py
+++ src/myfind/_meta.py
@@ -1,3 +1,7 @@
+from importlib.metadata import version, PackageNotFoundError
 
-VERSION = '0.0.1'
+try:
+    VERSION = version('myfind')
+except PackageNotFoundError:
+    VERSION = "0.0.0-dev"
 MATCHED_FILE_NAME = '.patterns'
